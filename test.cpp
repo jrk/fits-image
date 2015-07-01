6abe0e6621fba1f595520db1f7727b368c1c3c30
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2015-06-30 23:19:19 -0700

    Trivial test program

--- test.cpp
+++ test.cpp
@@ -1,18 +1,26 @@
-//#include <Halide.h>
-#include "fits_image.h"
-//#include "image_io.h"
-
+#include <Halide.h>
 using namespace Halide;
+
+#include "fits_image.h"
+#include "image_io.h"
 
 int main (int argc, char const *argv[])
 {
     assert(argc == 2);
 
-    buffer_t in = load_fits(argv[1]);
+    buffer_t inbuf = load_fits(argv[1]);
 
-    printf("Loaded FITS: %d x %d pixels\n", in.extent[0], in.extent[1]);
+    printf("Loaded FITS: %d x %d pixels\n", inbuf.extent[0], inbuf.extent[1]);
     
-    //Buffer inbuf(in);
+    Image<float> in(&inbuf);
+    Func out;
+    Var x, y;
+    
+    out(x,y) = in(x,y) / 256.f; // some random math
+    
+    Image<float> res = out.realize(in.width(), in.height());
+
+    save(res, "test.png");
 
     return 0;
 }
